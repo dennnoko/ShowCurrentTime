8b3fb881c582a8aac8d1f88dbe7f17e7d3079ade
でんのこ <dennnoko314@gmail.com>
2023-08-04 02:59:43 +0900

    Rewrite MainActivity using ViewBinding

--- app/src/main/java/com/example/viewtestapp/MainActivity.kt
+++ app/src/main/java/com/example/viewtestapp/MainActivity.kt
@@ -5,18 +5,20 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.example.viewtestapp.databinding.ActivityMainBinding
 import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+    //ViewBinding
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //ViewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        //イベントリスナーを利用してみる。ラムダ式を利用
-        val btnCurrent = findViewById<Button>(R.id.btnCurrent) //イベントリスナーの登録先のViewを予め取得
-        btnCurrent.setOnClickListener({v: View -> //setOnClickListenerメソッドを呼び出し、ボタンにクリックリスナーを登録している。クリックリスナーとしてラムダ式が使用されている。
-            val txtResult = findViewById<TextView>(R.id.textResult) //Viewの取得
-            txtResult.text = Date().toString() //取得したViewのtextプロパティに値を代入
-        })
+        binding.btnCurrent.setOnClickListener {
+            binding.textResult.text = Date().toString()
+        }
     }
 }
